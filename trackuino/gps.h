4e53b825c4497388d83cc36cc2129d3908c1aadd
anqixxx <74876670+anqixxx@users.noreply.github.com>
2021-03-07 12:49:43 -0800

    Removed from Main Code
    Removed from main code as we are using a different type of GPS

--- trackuino/gps.h
+++ trackuino/gps.h
@@ -14,7 +14,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
+/*
 #ifndef __GPS_H__
 #define __GPS_H__
 
@@ -36,3 +36,4 @@ bool gps_decode(char c);
 void gps_reset_parser();
 
 #endif
+*/
